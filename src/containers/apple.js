82b10edf21290f40b1a38759288a10246613b409
weipengzhou <492020314@qq.com>
2016-12-12 23:24:13 +0800

    添加显示组件的动态渲染

--- src/containers/apple.js
+++ src/containers/apple.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import { connect} from 'react-redux';
 import '../styles/apple.scss';
+import AppleItem from '../components/AppleItem'
 
 class AppleBusket extends React.Component{
 
     render(){
+        let {state} = this.props;
+        let mockState = {
+            isPicking : false,
+            newAppleId: 3,
+            apples: [
+                {
+                    id: 1,
+                    weight: 235,
+                    isEaten: true
+                },
+                {
+                    id: 2,
+                    weight: 256,
+                    isEaten: false
+                }
+            ]
+        };
+         state = mockState;
+         let stats = {
+            appleNow: {
+                quantity: 0,
+                weight: 0
+            },
+            appleEaten: {
+                quantity: 0,
+                weight: 0
+            }
+        };
+        state.apples.map(apple=>{
+            let selector = apple.isEaten ? 'appleEaten':'appleNow';
+            stats[selector].quantity ++;
+            stats[selector].weight += apple.weight;
+        })
         return(
        <div className="appleBusket">
         <div className="title">苹果篮子</div>
@@ -12,16 +46,21 @@ class AppleBusket extends React.Component{
         <div className="stats">
             <div className="section">
                 <div className="head">当前</div>
-                <div className="content">0个苹果，0克</div>
+                <div className="content">
+                {stats.appleNow.quantity}
+                个苹果，
+                {stats.appleNow.weight}
+                克</div>
             </div>
             <div className="section">
                 <div className="head">已吃掉</div>
-                <div className="content">2个苹果，480克</div>
+                <div className="content">{stats.appleEaten.quantity}个
+                苹果，{stats.appleEaten.weight}克</div>
             </div>            
         </div>
                     
         <div className="appleList">
-            <div className="empty-tip">苹果篮子空空如也</div>
+            <div className="empty-tip">{state.apples.map(apple=><AppleItem state={apple}/>)}</div>
         </div>
         
         <div className="btn-div">
@@ -33,4 +72,9 @@ class AppleBusket extends React.Component{
         )
     }
 }
-export default AppleBusket
+function select(state){
+    return{
+        state:state.appleBusket
+    }
+}
+export default connect(select)(AppleBusket);
